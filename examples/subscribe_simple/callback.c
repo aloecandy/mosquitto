42ddbd07ef91c2609060c500a1d60991fb3b0373
Kyunghoon <aloecandy@gmail.com>
2019-06-03 20:07:33 +0900

    sub and save init

--- examples/subscribe_simple/callback.c
+++ examples/subscribe_simple/callback.c
@@ -1,24 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "mosquitto.h"
+#include <string.h>
+#include <time.h>
 
+FILE *fp;
+
+char fileAddr[256];
+time_t current_time;
+struct tm *struct_time;
+struct tm *prev_struct_time;
+
+int createFile(char* filename){
+	strcat(strcpy(fileAddr, getenv("HOME")), "/");
+	strcat(fileAddr, filename);
+	strcat(fileAddr,".csv");
+	fp=fopen(fileAddr,"w");
+	if(fp==NULL){
+        printf("Failed to open %s",fileAddr);
+		return -1;
+    }
+	else{
+		fprintf(fp, "topic,time,dust[ug/m^3]\n");
+		fclose(fp);
+		return 1;
+	}
+}
 int on_message(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *msg)
 {
-	printf("%s %s (%d)\n", msg->topic, (const char *)msg->payload, msg->payloadlen);
+	time(&current_time);
+	struct_time=localtime(&current_time);
+	if(prev_struct_time==NULL ||prev_struct_time->tm_mday!=struct_time->tm_mday){
+		prev_struct_time=struct_time;
+		char tmp[20];
+		sprintf(tmp,"dust_%02d%02d%02d",struct_time->tm_year%100,struct_time->tm_mon,struct_time->tm_mday);
+		createFile(tmp);
+	}
+	
+	printf("%s,%2d%2d%2d,%s\n", msg->topic,
+	  struct_time->tm_hour,struct_time->tm_min,struct_time->tm_sec,
+	  (const char *)msg->payload);
+	fp=fopen(fileAddr,"a");
+	if(fp==NULL){
+        printf("Failed to open %s",fileAddr);
+		return -1;
+    }
+	else{
+		fprintf(fp,"%s,%02d%02d%02d,%s\n", msg->topic,
+			struct_time->tm_hour,struct_time->tm_min,struct_time->tm_sec,
+			(const char *)msg->payload);
+		fclose(fp);
+	}
 	return 0;
 }
 
 
 int main(int argc, char *argv[])
 {
+	
 	int rc;
-
+	
 	mosquitto_lib_init();
 
 	rc = mosquitto_subscribe_callback(
 			on_message, NULL,
-			"irc/#", 0,
-			"test.mosquitto.org", 1883,
+			"dust/#", 0,
+			"ethernetcard.dlinkddns.com", 1883,
 			NULL, 60, true,
 			NULL, NULL,
 			NULL, NULL);
@@ -26,9 +73,9 @@ int main(int argc, char *argv[])
 	if(rc){
 		printf("Error: %s\n", mosquitto_strerror(rc));
 	}
-
+	
 	mosquitto_lib_cleanup();
-
+		
 	return rc;
 }
 
